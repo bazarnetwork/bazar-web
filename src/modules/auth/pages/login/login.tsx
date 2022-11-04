3111e12b2242d066361f688b39af9fc5755f32f7
Cristopher Paniagua <cpaniagua@cafetosoftware.com>
2022-11-03 20:16:45 -0600

    Changes icon and fixes for mobile

--- src/modules/auth/pages/login/login.tsx
+++ src/modules/auth/pages/login/login.tsx
@@ -4,11 +4,14 @@ import IconLogo from "../../../../assets/svg/icons/iconLogo";
 const Login: React.FC = () => {
   return (
     <div className="coming-soon">
-      <div className="coming-soon_footer">
-        <p className="coming-soon_footer_text">
+      <div className="coming-soon_content">
+        <p className="coming-soon_content_title">
+            Coming Soon
+        </p> 
+
+        <p className="coming-soon_content_text">
           We are working building the first sustainable network of buyers and sellers of sourced commodities.
         </p>
-
         <IconLogo></IconLogo>
       </div>
     </div>
